1535cd54eb90de20b39473150a92b0487e4c232f
karbawona <37221123+karbawona@users.noreply.github.com>
2019-04-29 18:31:37 +0200

    Initial Commit

---
+++ Tasks- Intern Egnyte/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tasks- Intern Egnyte
+//
+//  Created by Ola on 29/04/2019.
+//  Copyright © 2019 Aleksandra Klein. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tasks- Intern Egnyte/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tasks- Intern Egnyte
+//
+//  Created by Ola on 29/04/2019.
+//  Copyright © 2019 Aleksandra Klein. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
